a8b326fe664748b732b264b6d33f744dcd0afba3
萩原 望 <hagiwara.nozomu@geechs.com>
2022-08-23 01:12:41 +0900

    confluence

--- HelpDeveloper/src/page_action/page_action.confluence.js
+++ HelpDeveloper/src/page_action/page_action.confluence.js
@@ -18,9 +18,46 @@ class PageAction_Confluence {
     });
   }
 
-  static DoExtend(url) {
+  static async DoExtend(url) {
+    // body
     $("body").addClass("ner-extend-confluence");
 
+    // cover
+    let cover = document.createElement("div");
+    $(cover).addClass("screen-cover");
+    $("body").append(cover);
+    let coverOff = document.createElement("button");
+    coverOff.innerText = "x";
+    coverOff.onclick = function () {
+      $(cover).remove();
+      $(coverOff).remove();
+    };
+    $(coverOff).addClass("screen-cover-off");
+    $("body").append(coverOff);
+
+    // item
+    let currentURL = url;
+    const urlObserver = new MutationObserver(async function () {
+      // URL変更に対応
+      if (currentURL !== location.href) {
+        if (currentURL.includes("edit")) {
+          await CommonLib.delay(2);
+        }
+        currentURL = location.href;
+        PageAction_Confluence.ItemSetting();
+      }
+    });
+    urlObserver.observe(document, { childList: true, subtree: true });
+
+    // list
+    const listObserver = new MutationObserver(async function () {
+      PageAction_Confluence.ItemSetting();
+    });
+    listObserver.observe($(".e4yppja0").get(0), { childList: true, subtree: true });
+    PageAction_Confluence.ItemSetting();
+  }
+
+  static ItemSetting() {
     $(".e1wjl5rj4").each(function (index, value) {
       // current
       if (value.getAttribute("aria-current") === "page") {
@@ -28,11 +65,11 @@ class PageAction_Confluence {
       }
       // indent
       var pl = $(value).css("padding-left");
-      pl = parseInt(pl.replace("px", "")) / 2;
-      $(value).css("padding-left", pl);
+      pl = parseInt(pl.replace("px", ""));
+      if ((pl - 8) % 24 == 0) {
+        pl /= 2;
+      }
+      $(value).css("padding-left", pl + "px");
     });
-
-    var attr = $(".e1wjl5rj4").get(0).getAttribute("aria-current");
-    attr === "page";
   }
 }
